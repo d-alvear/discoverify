191dc132e20ff78bd96397b1cce6a1407ebf5aee
d-alvear <deandraalvear@gmail.com>
2020-10-01 21:15:58 -0700

    added gitignore, generated first dataset for building database

---
+++ notebooks/secret.py
@@ -0,0 +1,18 @@
+sql_credentials = {
+    'database':'postgres',
+    'user':'postgres',
+    'password':'quartzite!',
+    'host':'/cloudsql/friendship-playlist-104:us-west2:spotify-db-lite'
+}
+
+spotify_credentials = {
+'client_id': 'b05f690e17ba4b758d92a214cfbc7f24',
+'client_secret':'ff913219c68f44a292bfe86922d19d8b',
+'redirect_uri':'http://127.0.0.1'
+}
+
+google_credentials = {
+'bucket_name':'friendship-playlist-104.appspot.com',
+'bucket_folder':'audio-files/',
+'local_files':'local-files'
+}
